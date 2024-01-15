b381d837068ebcb0bb26531111c6fdbdb56c35d0
Bita Fotovvat <bita.ftvt@gmail.com>
2024-01-15 17:43:47 -0500

    final merge

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -1,57 +1,278 @@
 
-const api_key= "e6882fd0-adc7-4899-9868-cd311df69876";
-
 export default class BandsiteAPI {
     constructor(baseURL){
         this.baseURL = baseURL;
-        // this.api_key = "e6882fd0-adc7-4899-9868-cd311df69876";
     }
 
+    async postComments(album){
+        try{
+            await axios.post(this.baseURL, album)
 
-    //get comments
+        }catch(error){
+            console.error("error", error.response.data);
+        }
+    }
+
     async getComments(){
         try{
             const response = await axios.get(this.baseURL);
             const comments = response.data;
-            console.log(comments); //remove later
 
+            comments.sort((a, b) => b.timestamp - a.timestamp)
 
-            const commentsEl = document.querySelector(".comments__container");
-            comments.forEach((cmnt)=>{
-                const nameElem = document.createElement("p");
-                nameElem.textContent = cmnt.name;
-                nameElem.classList.add("name");
-                commentsEl.appendChild(nameElem);
-
-                const dateElem = document.createElement("p");
-                dateElem.textContent = cmnt.timestamp;
-                dateElem.classList.add("date");
-                commentsEl.appendChild(dateElem);            
-
-                const cmntElem = document.createElement("p");
-                cmntElem.textContent = cmnt.comment;
-                cmntElem.classList.add("comment");
-                commentsEl.appendChild(cmntElem);  
+                const commentsContainer = document.querySelector(".comments__container");
                 
-                return comments;
+                const parentDiv = document.createElement('div');
+                parentDiv.classList.add("comments__card--parent");
+                commentsContainer.appendChild(parentDiv);
 
+                comments.forEach((cmnt)=>{
+                const commentCardEl = document.createElement("div"); 
+                commentCardEl.classList.add("comments__card");
+                parentDiv.appendChild(commentCardEl);
+
+                const commentCardParent1 = document.createElement("div"); 
+                commentCardParent1.classList.add("comments__card--parent1");
+                commentCardEl.appendChild(commentCardParent1);
+
+                const commentCardParent2 = document.createElement("div"); 
+                commentCardParent2.classList.add("comments__card--parent2");
+                commentCardEl.appendChild(commentCardParent2);
+
+                const userIcon = document.createElement("div");
+                userIcon.classList.add("comments__icon");
+                commentCardParent1.appendChild(userIcon);
+
+                const commentCardCont1 = document.createElement("div"); 
+                commentCardCont1.classList.add("comments__card--cont1");
+                commentCardParent2.appendChild(commentCardCont1);
+
+                const userName = document.createElement("p");
+                userName.classList.add("comments__username");
+                userName.innerText = cmnt.name;
+                commentCardCont1.appendChild(userName);
+
+                const userDate2 = document.createElement("p");
+                userDate2.classList.add("comments__date");
+                const timestamp = cmnt.timestamp;
+                const date = new Date(timestamp);
+                const formattedDate = `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`;
+
+                userDate2.innerText = formattedDate;
+
+                commentCardCont1.appendChild(userDate2);
+
+                const commentCardCont2 = document.createElement("div"); 
+                commentCardCont2.classList.add("comments__card--cont2");
+                commentCardParent2.appendChild(commentCardCont2);
+
+                const userComment = document.createElement("p");
+                userComment.classList.add("comments__text");
+                userComment.innerText = cmnt.comment;
+                commentCardCont2.appendChild(userComment);
+
+                return comments;
             });
 
         }catch(error){
-            console.error("error");
+            console.error("Error:", error);
+            return [];
         }
     }
 
-    //post comments
-    async postComments(word){
+
+    async getShows(){
         try{
-            await axios.post(this.baseURL, word)
+            const showsresponse = await axios.get(this.baseURL);
+            console.log(showsresponse);
+            const shows = showsresponse.data;
+
+            const showsContainer = document.querySelector(".shows");
+     
+            const showsHeader = document.createElement('h2');
+            showsHeader.classList.add('shows__header');
+            showsContainer.appendChild(showsHeader);
+            showsHeader.innerText = ('Shows');
+
+            const headWrapper = document.createElement('div');
+            headWrapper.classList.add('shows__headwrapper');
+            showsContainer.appendChild(headWrapper);
+
+            headWrapper.appendChild(showsHeader);
+
+            const showWrapper = document.createElement('div');
+            showWrapper.classList.add('shows__showwrapper');
+            showsContainer.appendChild(showWrapper);
+
+            const showsCardTD = document.createElement("div");
+            showsCardTD.classList.add("shows__cardtabletdesktop");
+            showsContainer.appendChild(showsCardTD);
+
+            const titleDateTD = document.createElement("p");
+            titleDateTD.classList.add("shows__titletabletdesktop");
+            titleDateTD.innerText = ("DATE");
+            showsCardTD.appendChild(titleDateTD);
+
+            const titleVenueTD = document.createElement("p");
+            titleVenueTD.classList.add("shows__titletabletdesktop");
+            titleVenueTD.innerText = ("VENUE");
+            titleVenueTD.setAttribute("id", "title2");
+            showsCardTD.appendChild(titleVenueTD);
+
+            const titleLocationTD = document.createElement("p");
+            titleLocationTD.classList.add("shows__titletabletdesktop");
+            titleLocationTD.innerText = ("LOCATION");
+            titleLocationTD.setAttribute("id", "title3");
+            showsCardTD.appendChild(titleLocationTD);
+
+            showWrapper.appendChild(showsCardTD);
+
+            function addSelectedState(event) {
+            document.querySelectorAll('.shows__card').forEach(card => {
+            card.classList.remove('selected');
+            card.classList.add('notselected');
+            });
+
+            const clickedShow = event.currentTarget.closest('.shows__card');
+
+             if (clickedShow) {
+             clickedShow.classList.add('selected');
+             clickedShow.classList.remove('notselected');
+             }
+
+             document.body.classList.add('section-selected');
+}
+
+    function formatDate(timestamp) {
+    const date = new Date(timestamp);
+    const options = { weekday: 'short', month: 'short', day: '2-digit', year: 'numeric' };
+    return date.toLocaleDateString('en-US', options).replace(/,/g, '');
+    }
+
+    function displayShows(shows,index){
+    
+    const showsCard = document.createElement("div");
+    showsCard.classList.add("shows__card");
+    showsCard.setAttribute("id", index);
+    showsContainer.appendChild(showsCard);
+
+    const titleDate = document.createElement("p");
+    titleDate.classList.add("shows__title");
+    titleDate.innerText = ("DATE");
+    showsCard.appendChild(titleDate);
+
+    const showsDate = document.createElement("p");
+    showsDate.classList.add("shows__date");
+    const formattedDate = formatDate(shows.date);
+    showsDate.innerText = formattedDate;
+    
+    showsDate.setAttribute("id", index);
+    showsCard.appendChild(showsDate);
+
+
+
+
+    const titleVenue = document.createElement("p");
+    titleVenue.classList.add("shows__title");
+    titleVenue.innerText = ("VENUE");
+    showsCard.appendChild(titleVenue);
+
+    const showsVenue = document.createElement("p");
+    showsVenue.classList.add("shows__venue");
+    showsVenue.innerText = (shows.place);
+    showsVenue.setAttribute("id", index);
+    showsCard.appendChild(showsVenue);
+
+    const titleLocation = document.createElement("p");
+    titleLocation.classList.add("shows__title");
+    titleLocation.innerText = ("LOCATION");
+    showsCard.appendChild(titleLocation);
+
+    const showsLocation = document.createElement("p");
+    showsLocation.classList.add("shows__location");
+    showsLocation.innerText = (shows.location);
+    showsLocation.setAttribute("id", index);
+    showsCard.appendChild(showsLocation);
+
+
+    const showsButton = document.createElement('button');
+    showsButton.type = ("submit");
+    showsButton.id = ("submit");
+    showsButton.classList.add("shows__button");
+    showsButton.innerText = ("BUY TICKETS");
+    showsCard.appendChild(showsButton);
+
+
+    showsButton.addEventListener('click', addSelectedState);
+
+    showWrapper.appendChild(showsCard);
+}
+
+
+    function renderAllShows(allShows){
+
+    allShows.forEach((shows,index) => {
+    displayShows(shows,index);
+
+  });
+}
+
+renderAllShows(shows);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         }catch(error){
-            console.error("error");
+            console.error("Error:", error);
+            return [];
         }
-
     }
+
+
+
+
+
+
+
+///////////////////////////////////////shows
+
 
 }
 
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,34 +1,16 @@
-//new sprint-3 content//////////////
 
 import BandsiteAPI from "./band-site-api.js";
 
-const api_key= "e6882fd0-adc7-4899-9868-cd311df69876";
-const commentsURL = `https://project-1-api.herokuapp.com/comments?api_key=${api_key}`;
+const api_key= "f735125b-025d-4208-8ff7-1e6deeae76c9";
+const baseURL = "https://project-1-api.herokuapp.com/"
+const commentsURL = `${baseURL}comments?api_key=${api_key}`;
 const comments = new BandsiteAPI(commentsURL);
 comments.getComments();
-
-///////////////////////////
-const fixedComments =[
-  {
-    name: "Connor Walton",
-    timestamp: "02/17/2021",
-    comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-  },
-  {
-    name: "Emilie Beach",
-    timestamp: "01/09/2021",
-    comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-  },
-  {
-    name: "Miles Acosta",
-    timestamp: "12/20/2020",
-    comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-  }
-];
 
 
 
 const commentsContainer = document.querySelector(".comments__container");
+
 
 const commentsFormWrapper = document.createElement('div');
 commentsFormWrapper.classList.add('comments__formwrapper');
@@ -38,7 +20,6 @@ const commentsHeader = document.createElement('h2');
 commentsHeader.classList.add('comments__header');
 commentsFormWrapper.appendChild(commentsHeader);
 commentsHeader.innerText = ('Join the Conversation');
-
 
 const commentsForm = document.createElement('form');
 commentsForm.classList.add("comments__form");
@@ -50,7 +31,6 @@ const formWrapper = document.createElement('div');
 formWrapper.classList.add('form__wrapper');
 commentsForm.appendChild(formWrapper);
 
-
 const userImage = document.createElement("div");
 userImage.classList.add("comments__userimage");
 
@@ -59,7 +39,6 @@ formWrapper.appendChild(userImage);
 const formColumnsWraper = document.createElement("div");
 formColumnsWraper.classList.add("comments__formcolumnswrapper");
 formWrapper.appendChild(formColumnsWraper);
-
 
 const formColumnOne = document.createElement('div');
 formColumnOne.classList.add('form__columnone');
@@ -78,7 +57,6 @@ formInputOne.id = ("name");
 formInputOne.placeholder = ("Enter your name");
 formColumnOne.appendChild(formInputOne);
 
-
 const formColumnTwo = document.createElement('div');
 formColumnTwo.classList.add('form__columntwo');
 formColumnsWraper.appendChild(formColumnTwo);
@@ -96,7 +74,6 @@ formInputTwo.id = ("comment");
 formInputTwo.placeholder = ("Add a new comment");
 formColumnTwo.appendChild(formInputTwo);
 
-
 const submitButton = document.createElement('button');
 submitButton.type = ("submit");
 submitButton.id = ("submit");
@@ -104,104 +81,44 @@ submitButton.classList.add("comments__button");
 submitButton.innerText = ("COMMENT");
 formColumnsWraper.appendChild(submitButton);
 
+const albumFormEl = document.getElementById("cmntForm");
 
 const parentDiv = document.createElement('div');
 parentDiv.classList.add("comments__card--parent");
 commentsContainer.appendChild(parentDiv);
 
-function displayComments(album) {  
-
-const commentCardEl = document.createElement("div"); 
-commentCardEl.classList.add("comments__card");
-parentDiv.appendChild(commentCardEl);
-
-
-const commentCardParent1 = document.createElement("div"); 
-commentCardParent1.classList.add("comments__card--parent1");
-commentCardEl.appendChild(commentCardParent1);
-
-const commentCardParent2 = document.createElement("div"); 
-commentCardParent2.classList.add("comments__card--parent2");
-commentCardEl.appendChild(commentCardParent2);
-
-
-
-const userIcon = document.createElement("div");
-userIcon.classList.add("comments__icon");
-commentCardParent1.appendChild(userIcon);
-
-
-
-const commentCardCont1 = document.createElement("div"); 
-commentCardCont1.classList.add("comments__card--cont1");
-commentCardParent2.appendChild(commentCardCont1);
-
-const userName = document.createElement("p");
-userName.classList.add("comments__username");
-userName.innerText = album.name;
-commentCardCont1.appendChild(userName);
-
-const userDate2 = document.createElement("p");
-userDate2.classList.add("comments__date");
-userDate2.innerText = album.timestamp;
-commentCardCont1.appendChild(userDate2);
-
-
-const commentCardCont2 = document.createElement("div"); 
-commentCardCont2.classList.add("comments__card--cont2");
-commentCardParent2.appendChild(commentCardCont2);
-
-const userComment = document.createElement("p");
-userComment.classList.add("comments__text");
-userComment.innerText = album.comment;
-commentCardCont2.appendChild(userComment);
-}
-
-function renderAllComments(allComments){
-
-  parentDiv.innerHTML = "";
-
-allComments.forEach((album) => {
-  displayComments(album);
-});
-}
-
-renderAllComments(fixedComments);
-
-
-
-
-
-const albumFormEl = document.getElementById("cmntForm");
-
-albumFormEl.addEventListener("submit", (event) => {
-
+albumFormEl.addEventListener("submit", async (event) => {
 event.preventDefault();
 
 const userName = event.target.elements.username.value;
 const userComment = event.target.elements.comment.value;
 
-const timestamp = new Date().toLocaleDateString('en-US', {
-month: '2-digit',
-day: '2-digit',
-year: 'numeric'
-});
 
 const album = {
-name: userName,
-comment: userComment,
-timestamp: timestamp,
-icon: ""
+    name: userName,
+    comment: userComment,
 };
 
-fixedComments.unshift(album);
-renderAllComments(fixedComments);
+
+await comments.postComments(album);
 
 albumFormEl.reset();
 
-//call the POST request method here
-comments.getComments(album);
+
+const parentDiv = document.querySelector(".comments__card--parent");
+parentDiv.innerHTML = '';
 
 });
 
-
+const newComment = {
+    name: userName,
+    comment: userComment,
+    timestamp: new Date().getTime(), 
+  };
+  
+ 
+  const commentCardEl = document.createElement("div"); 
+  commentCardEl.classList.add("comments__card");
+  parentDiv.appendChild(commentCardEl);
+  
+  displayComment(newComment, commentCardEl);
